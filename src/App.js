b50ecb68ae6714c4224572109280078d635eafc2
Foysal Ahamed Badsha <you@example.com>
2022-04-30 01:41:18 +0600

    blog and notfound complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './componants/Blogs/Blogs';
 import Header from './componants/Header/Header';
 import Home from './componants/Home/Home';
 import Login from './componants/Login/Login';
+import NotFound from './componants/NotFound/NotFound';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Header> </Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
