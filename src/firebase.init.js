27b11ef9992a1e2e3e3318f3d8c4d76a1c6a7415
Foysal Ahamed Badsha <you@example.com>
2022-04-30 20:15:48 +0600

    environment variable fixed

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBtoyZKXXmnrLZ_6hFP5ru-S-_ZR9CFCn8",
-    authDomain: "laptop-werehouse-website.firebaseapp.com",
-    projectId: "laptop-werehouse-website",
-    storageBucket: "laptop-werehouse-website.appspot.com",
-    messagingSenderId: "729787618935",
-    appId: "1:729787618935:web:3ec0424a99396d88111872"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
